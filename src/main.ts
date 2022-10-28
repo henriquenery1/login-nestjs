9099378d9f5ab84543737176bf4f6f94ac41d15e
Henrique Nery <henriquenery07@gmail.com>
2022-10-28 15:45:55 -0300

    feat: add { whitelist: true } on ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { withLatestFrom } from 'rxjs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({whitelist: true}));
   await app.listen(3000);
 }
 bootstrap();
